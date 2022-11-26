a2f51a6cd5876399ec41a12bfcd7a2c7280cec25
igor_rush <rick.c137.box@gmail.com>
2022-11-26 19:48:15 +0300

    final version

---
+++ config.py
@@ -0,0 +1,13 @@
+from locale import currency
+
+
+TOKEN = '5896761120:AAFohfYcL0_M0rf-Jrx7BaFrcT15n6giy3s'
+
+currencies = {
+  'доллар' : 'USD',
+  'рубль' : 'RUB',
+  'евро' : 'EUR',
+  'фунт' : 'GBP',
+  'йена' : 'JPY',
+  'юань' : 'CNY'
+ }
